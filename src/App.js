1172b3cc6fab0b6d600a8cd6469f2e18099995ce
sssulp <91898509+sssulp@users.noreply.github.com>
2022-01-15 12:48:10 -0500

    created search function for API and event/handle listeners for userInput + submission

--- src/App.js
+++ src/App.js
@@ -6,27 +6,48 @@ import './App.css';
 
 function App() {
 
-  const [anime, setAnime] = useState([]);
+  const [ anime, setAnime ] = useState([]);
+  const [ userInput, setUserInput ] = useState("");
+  const [ searchTerm, setSearchTerm ] = useState("");
 
-  // useEffect to call API
+  // make an API call to Jikan API
   useEffect( () => {
     axios({
-      url: 'https://api.jikan.moe/v3/top/anime',
+      url: 'https://api.jikan.moe/v3/search/anime?',
       method: "GET",
       dataResponse: "json",
       params: {
-        page: 1,
-        subtype: "tv",
-        format: "json" 
-      }
-    }).then( (response) => {
-      console.log(response);
+        q: searchTerm,
+        order_by: 'title',
+        sort: 'dsc',
+        format: 'json',
+      },
+    }).then( (res) => {
+      setAnime(res.data.top);
     });
-  }, []);
+  }, [searchTerm]);
+
+  // create function that tracks the user's input on the searchbar
+  const handleInput = (event) => {
+    console.log('is this working?', event.target.value);
+    setUserInput(event.target.value);
+  }
+
+  // create function that handles user's SUBMITTION of their input in the search bar
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchTerm(userInput);
+  }
 
   return (
     <div className="App">
       <Header />
+      <main>
+        <form onSubmit={ handleSubmit }>
+          <input type="text" id="search" placeholder="Naruto" onChange={ handleInput } value={ userInput }/>
+          <button>Search</button>
+        </form>
+      </main>
       <Footer />
     </div>
   );
