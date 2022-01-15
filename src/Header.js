d99fc2f01a69413e059e0074a69920af800a685d
Andre <andre.shotwell@gmail.com>
2022-01-15 11:47:10 -0500

    created components header.js, footer.js

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+// Header.js
+
+const Header = () => {
+    return (
+        <header>
+            <div className="wrapper">
+                <h1>AnimeSeeker</h1>
+                <p className="title-slogan">seek your next binge.</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
