8c973207390e33ab7574d79a83c7b7c31ea3a9ba
sssulp <91898509+sssulp@users.noreply.github.com>
2023-01-05 10:38:58 -0500

    migrated to jikan v4 + small style update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 import Header from './components/Header';
+// import Search from './components/Search';
+// import TopAnime from './components/TopAnime';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -17,15 +19,27 @@ function App() {
   const [ anime, setAnime ] = useState([]);
   const [ userInput, setUserInput ] = useState("");
   const [ searchTerm, setSearchTerm ] = useState("");
-
+  
   // font awesome button icon
   const questionMark = <FontAwesomeIcon icon={ faQuestionCircle }/>
   const [ openModal, setOpenModal ] = useState(false);
-
-  // make an API call to Jikan API
+  
+  // create function that tracks the user's input on the searchbar
+  const handleInput = (event) => {
+    setUserInput(event.target.value);
+  }
+    
+  // create function that handles user's SUBMITTION of their input in the search bar
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchTerm(userInput);
+    setUserInput("");
+  } 
+  
+  // call api in descending order 
   useEffect( () => {
     axios({
-      url: 'https://api.jikan.moe/v3/search/anime?',
+      url: 'https://api.jikan.moe/v4/anime',
       method: "GET",
       dataResponse: "json",
       params: {
@@ -35,7 +49,7 @@ function App() {
         format: 'json',
       },
     }).then( (res) => {
-      setAnime(res.data.results);
+      setAnime(res.data.data);
     }).catch( (error) => {
       if (error.message) {
         alert('Sorry, that anime does not exist! Please look up another one')
@@ -45,62 +59,82 @@ function App() {
     });
   }, [searchTerm]);
 
-  // create function that tracks the user's input on the searchbar
-  const handleInput = (event) => {
-    setUserInput(event.target.value);
-  }
+  // CALL TOP RESULTS
+  // useEffect(() => {
+  //   axios({
+  //     method: "GET",
+  //     url: 'https://api.jikan.moe/v4/top/anime',
+  //     dataResponse: 'json',
+  //     params: {
+  //       page: 1,
+  //       subtype: 'tv',
+  //       format: "json",
+  //     }
+  //   }).then((res) => {
+  //     const topAnime = res.data.data;
+  //     const topResults = [];
 
-  // create function that handles user's SUBMITTION of their input in the search bar
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setSearchTerm(userInput);
-    setUserInput("");
-  } 
+  //     topAnime.map((results) => {
+  //       return topResults.push({
+  //         animeName: results.title_english,
+  //         animeImage: results.images.jpg.image_url,
+  //         animeSynopsis: results.synopsis,
+  //       });
+  //     })
+
+  //     setAnime(topResults)
+  //   })
+  // }, [])
 
   return (
     <div className="App">
       <Header />
       <main>
-        <div className='wrapper'>
+        <div className="wrapper">
           <form onSubmit={ handleSubmit }>
             <input type="text" id="search" placeholder="Violet Evergarden" label="" alt="Search your anime here" onChange={ handleInput } value={ userInput }/>
-
+            
             <button className="search-button">Search</button>
-          </form>
-
-          <button className='how-to' onClick={ () => setOpenModal(true) } aria-label="Click to see how the site works!">{ questionMark }</button>
-            <Modal 
-              transparent={true}
-              isOpen={ openModal } 
-              onRequestClose={ () => setOpenModal(false)}
-              style={
-                {
-                  content: {
-                    textAlign: 'center',
-                    height: '250px',
+            <button className='how-to' onClick={ () => setOpenModal(true) } aria-label="Click to see how the site works!">{ questionMark }</button>
+              <Modal 
+                transparent={true}
+                isOpen={ openModal } 
+                onRequestClose={ () => setOpenModal(false)}
+                style={
+                  {
+                    content: {
+                      justifySelf: 'center',
+                      textAlign: 'center',
+                      height: '20vmin',
+                      width: '75vmin',
+                      top: '25%',
+                      left: '50%',
+                      transform: 'translate(-50%, -50%)',
+                    }
                   }
-                }
               }>
-              <h2>How to use the search</h2>
-              <p>Enter the title of the anime you want to look up in the search bar. And hit search. Animes will populate below the header splash page by descending order. Powered by the Jikan API.</p>
-              <div>
-                <button className='close-button' onClick={ () => setOpenModal(false) }>Close</button>
-              </div>
-            </Modal>
-
-          <section>
-            {anime.map((show) => {
-              return (
-                <div className='main-display' key={show.mal_id}>
-                  <img src={show.image_url} alt={show.title}/>
-                  <h2>{show.title}</h2>
-                  <p className="anime-synopsis">About: {show.synopsis}</p>
+                <h2>How to use the search</h2>
+                <p>Enter the title of the anime you want to look up in the search bar. And hit search. Animes will populate below the header splash page by descending order. Powered by the Jikan API.</p>
+                <div>
+                  <button className='close-button' onClick={ () => setOpenModal(false) }>Close</button>
                 </div>
-              )
-            })}
-          </section>
+              </Modal>
+          </form>
         </div>
-      </main>
+
+
+        <section>
+          {anime.map((show) => {
+            return (
+              <div className='main-display' key={show.mal_id}>
+                <img src={show.images.jpg.image_url} alt={show.title}/>
+                <h2>{show.title}</h2>
+                <p className="anime-synopsis">About: {show.synopsis}</p>
+              </div>
+            )
+          })}
+        </section>
+       </main>
       <Footer />
     </div>
   );
